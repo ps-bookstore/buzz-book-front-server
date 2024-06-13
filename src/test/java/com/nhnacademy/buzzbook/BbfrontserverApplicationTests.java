a8bdc3114ee5d3637d014b6d4a8287921125dd9e
김성호 <120389368+Banana-Master@users.noreply.github.com>
2024-06-13 10:56:00 +0900

    🎉 start bb

---
+++ src/test/java/com/nhnacademy/buzzbook/BbfrontserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.buzzbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbfrontserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
