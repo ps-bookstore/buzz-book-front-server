80d00df9f723e552e850eaf5365b73f3555135cf
김성호 <120389368+Banana-Master@users.noreply.github.com>
2024-06-19 10:01:28 +0900

    style 장바구니 목록

---
+++ src/main/java/store/buzzbook/front/entity/product/Product.java
@@ -0,0 +1,12 @@
+package store.buzzbook.front.entity.product;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Product {
+	private String id;
+	private String name;
+	private int price;
+	// private int
+
+}
