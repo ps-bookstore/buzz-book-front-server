ae765da91ce911c694de83f2c706af8b97888dd2
ParkSeolDev <parkseol.dev@gmail.com>
2024-06-22 14:20:53 +0900

    chore: 수정

--- src/main/java/store/buzzbook/front/dto/payment/toss/CustomerIdentityNumber.java
+++ src/main/java/store/buzzbook/front/dto/payment/toss/CustomerIdentityNumber.java
@@ -1,6 +1,12 @@
 package store.buzzbook.front.dto.payment.toss;
 
 // 명세서에 없는 객체
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class CustomerIdentityNumber {
 	private CustomerMobilePhone customerMobilePhone;
 	private String businessRegistrationNumber;
