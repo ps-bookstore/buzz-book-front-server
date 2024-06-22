030bea60c7b6d928a92828beb856c173814b3095
ParkSeolDev <parkseol.dev@gmail.com>
2024-06-22 13:40:42 +0900

    chore: 수정

--- src/main/java/store/buzzbook/front/controller/order/OrderController.java
+++ src/main/java/store/buzzbook/front/controller/order/OrderController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import store.buzzbook.front.dto.order.CreateOrderRequest;
 import store.buzzbook.front.dto.order.ReadOrderResponse;
 import store.buzzbook.front.dto.order.WrappingResponse;
-import store.buzzbook.front.dto.payment.PaymentConfirmationRequest;
 import store.buzzbook.front.dto.user.AddressInfo;
 import store.buzzbook.front.dto.user.UserInfo;
 
@@ -22,18 +21,14 @@ public class OrderController {
     public String order(Model model) {
         model.addAttribute("page", "order");
         model.addAttribute("title", "주문하기");
-        // todo 객체 넣기
         model.addAttribute("myInfo", UserInfo.builder().name("PS").email("a@a.com").contactNumber("11111111111").build());
         List<AddressInfo> addressInfos = new ArrayList<>();
         addressInfos.add(AddressInfo.builder().id(1).addressName("우리집").build());
         model.addAttribute("addressInfos", addressInfos);
         model.addAttribute("createOrderRequest", new CreateOrderRequest());
-        // List<CreateOrderDetailRequest> createOrderDetailRequests = new ArrayList<>();
-        // model.addAttribute("createOrderDetailRequestList", createOrderDetailRequests);
         List<WrappingResponse> packages = new ArrayList<>();
         packages.add(WrappingResponse.builder().paper("신문지").price(1000).build());
         model.addAttribute("packages", packages);
-        model.addAttribute("paymentConfirmationRequest", new PaymentConfirmationRequest());
 
         return "index";
     }
