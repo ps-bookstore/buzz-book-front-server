447817d03be26a97b7db79403be6df22a152901c
Heldennar <krain7127@gmail.com>
2024-06-25 20:15:15 +0900

    refactor : import제거

--- src/main/java/store/buzzbook/front/client/user/UserRestClient.java
+++ src/main/java/store/buzzbook/front/client/user/UserRestClient.java
@@ -1,27 +1,11 @@
 package store.buzzbook.front.client.user;
 
-import java.util.Objects;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.client.RestClient;
 
-import lombok.extern.slf4j.Slf4j;
-import store.buzzbook.front.common.exception.user.UserAlreadyExistsException;
-import store.buzzbook.front.common.exception.user.UserNotFoundException;
 import store.buzzbook.front.dto.user.LoginUserResponse;
 import store.buzzbook.front.dto.user.RegisterUserApiRequest;
-import store.buzzbook.front.dto.user.RegisterUserRequest;
 import store.buzzbook.front.dto.user.RegisterUserResponse;
 import store.buzzbook.front.dto.user.UserInfo;
 
