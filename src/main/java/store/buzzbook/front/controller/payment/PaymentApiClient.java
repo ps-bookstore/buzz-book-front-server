a854d38421892901f46cfc733d318cbe11834591
s49e <dydqja1ghk@hanmail.net>
2024-07-02 20:09:55 +0900

    feat : 결제 조회, 취소

--- src/main/java/store/buzzbook/front/controller/payment/PaymentApiClient.java
+++ src/main/java/store/buzzbook/front/controller/payment/PaymentApiClient.java
@@ -1,17 +1,18 @@
 package store.buzzbook.front.controller.payment;
 
-import java.io.IOException;
-import java.net.http.HttpResponse;
-
 import org.json.simple.JSONObject;
 import org.springframework.http.ResponseEntity;
 
 public interface PaymentApiClient {
 	String getPayType();
-	ResponseEntity<JSONObject> confirm(String request) throws Exception;
-	HttpResponse<String> cancel(String paymentKey, String cancelReason) throws IOException, InterruptedException;
 	default boolean matchPayType(String payType) {
 		return payType.trim().equals(getPayType());
 	}
+
+	ResponseEntity<JSONObject> confirm(String request) throws Exception;
+
+	ResponseEntity<JSONObject> cancel(String paymentKey, String cancelReason);
+
 	ResponseEntity<JSONObject> read(String paymentKey);
+
 }
