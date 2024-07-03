a5359b97fe90e3d9998b98591f11f8e12a6ff668
Heldennar <krain7127@gmail.com>
2024-07-03 11:06:29 +0900

    fix : login 로직 오류 디버깅

--- src/main/java/store/buzzbook/front/dto/jwt/AuthRequest.java
+++ src/main/java/store/buzzbook/front/dto/jwt/AuthRequest.java
@@ -1,14 +1,20 @@
 package store.buzzbook.front.dto.jwt;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthRequest {
-	private String username;
+	private String loginId;
 	private String role;
+	private Long userId;
+	private String accessToken;
+	private String refreshToken;
 }
 
