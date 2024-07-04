851b6b16e3866a51bc0465b28d544b040655ef65
huijun98 <heej1998@gmail.com>
2024-07-04 09:49:46 +0900

    fix : 모델 불러오는 오류 임시수정

--- src/main/java/store/buzzbook/front/dto/product/ProductUpdateRequest.java
+++ src/main/java/store/buzzbook/front/dto/product/ProductUpdateRequest.java
@@ -1,15 +1,20 @@
 package store.buzzbook.front.dto.product;
 
 import lombok.Builder;
+import lombok.Data;
 
 @Builder
+@Data
 public class ProductUpdateRequest {
-	private int stock;
+
+	private	int stock;
 	private int price;
-	private String productName;
+	private String forwardDate;
+	private int score;
 	private String thumbnailPath;
-	private String stockStatus;
 	private int categoryId;
+	private String productName;
 	private String description;
+	private String stockStatus;
 
 }
