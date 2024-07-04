323964ec34c2011856c5aee2fcd08277e1d6adca
park <qmzo553@gmail.com>
2024-07-04 14:49:23 +0900

    :seedling: feat: create point policy

--- src/main/java/store/buzzbook/front/controller/admin/point/AdminPointController.java
+++ src/main/java/store/buzzbook/front/controller/admin/point/AdminPointController.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 import store.buzzbook.front.client.point.PointClient;
+import store.buzzbook.front.dto.point.CreatePointPolicyRequest;
 import store.buzzbook.front.dto.point.PointPolicyResponse;
 
 @Controller
@@ -30,8 +33,15 @@ public class AdminPointController {
 
 	@GetMapping("/policies")
 	public String createPointPolicy(Model model) {
-
+		model.addAttribute("page", "create-point-policy");
 		return "admin/index";
+	}
+
+	@PostMapping("/policies")
+	public String createPointPolicy(@ModelAttribute CreatePointPolicyRequest request) {
+		pointClient.createPointPolicy(request);
+
+		return "redirect:/admin/points";
 	}
 
 }
