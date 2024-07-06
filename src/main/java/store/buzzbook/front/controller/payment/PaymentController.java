258e2899978cd8511555d38e7d7e15924908b648
ParkSeolDev <parkseol.dev@gmail.com>
2024-07-06 09:52:09 +0900

    fix: 비회원 결제내역 확인

--- src/main/java/store/buzzbook/front/controller/payment/PaymentController.java
+++ src/main/java/store/buzzbook/front/controller/payment/PaymentController.java
@@ -126,16 +126,29 @@ public class PaymentController {
 	}
 
 	@GetMapping("/mybilllogs")
-	public String myPayment(Model model, @RequestParam String orderId) throws Exception {
+	public String myPayment(Model model, @RequestParam String orderId, HttpServletRequest request) throws Exception {
 
-		ReadBillLogRequest request = new ReadBillLogRequest(orderId);
+		ReadBillLogRequest readBillLogRequest = new ReadBillLogRequest(orderId);
 
 		RestTemplate restTemplate = new RestTemplate();
 
 		HttpHeaders headers = new HttpHeaders();
 		headers.set("Content-Type", "application/json");
 
-		HttpEntity<ReadBillLogRequest> readBillLogRequestHttpEntity = new HttpEntity<>(request, headers);
+		HttpEntity<ReadBillLogRequest> readBillLogRequestHttpEntity = new HttpEntity<>(readBillLogRequest, headers);
+
+		Optional<Cookie> jwt = cookieUtils.getCookie(request, CookieUtils.COOKIE_JWT_ACCESS_KEY);
+		Optional<Cookie> refresh = cookieUtils.getCookie(request, CookieUtils.COOKIE_JWT_REFRESH_KEY);
+
+		if(jwt.isEmpty()|| refresh.isEmpty()) {
+			throw new UserTokenException();
+		}
+
+		String accessToken = String.format("Bearer %s", jwt.get().getValue());
+		String refreshToken = String.format("Bearer %s", refresh.get().getValue());
+
+		headers.set(CookieUtils.COOKIE_JWT_ACCESS_KEY, accessToken);
+		headers.set(CookieUtils.COOKIE_JWT_REFRESH_KEY, refreshToken);
 
 		ResponseEntity<List<ReadBillLogWithoutOrderResponse>> response = restTemplate.exchange(
 			String.format("http://%s:%d/api/payments/bill-logs", host, port), HttpMethod.POST,
@@ -148,26 +161,26 @@ public class PaymentController {
 		return "index";
 	}
 
-	// @GetMapping("/nonMemberBilllogs")
-	// public String nonMemberPayment(Model model, @RequestParam String orderId) throws Exception {
-	//
-	// 	ReadBillLogRequest readBillLogRequest = new ReadBillLogRequest(orderId);
-	//
-	// 	RestTemplate restTemplate = new RestTemplate();
-	//
-	// 	HttpHeaders headers = new HttpHeaders();
-	// 	headers.set("Content-Type", "application/json");
-	//
-	// 	HttpEntity<ReadBillLogRequest> readBillLogRequestHttpEntity = new HttpEntity<>(readBillLogRequest, headers);
-	//
-	// 	ResponseEntity<List<ReadBillLogWithoutOrderResponse>> response = restTemplate.exchange(
-	// 		String.format("http://%s:%d/api/payments/bill-logs", host, port), HttpMethod.POST,
-	// 		readBillLogRequestHttpEntity, new ParameterizedTypeReference<List<ReadBillLogWithoutOrderResponse>>() {
-	// 		});
-	//
-	// 	model.addAttribute("myBillLogs", response.getBody());
-	// 	model.addAttribute("page", "mybilllog");
-	//
-	// 	return "index";
-	// }
+	@GetMapping("/nonMemberBilllogs")
+	public String nonMemberPayment(Model model, @RequestParam String orderId) throws Exception {
+
+		ReadBillLogRequest readBillLogRequest = new ReadBillLogRequest(orderId);
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Content-Type", "application/json");
+
+		HttpEntity<ReadBillLogRequest> readBillLogRequestHttpEntity = new HttpEntity<>(readBillLogRequest, headers);
+
+		ResponseEntity<List<ReadBillLogWithoutOrderResponse>> response = restTemplate.exchange(
+			String.format("http://%s:%d/api/payments/bill-logs", host, port), HttpMethod.POST,
+			readBillLogRequestHttpEntity, new ParameterizedTypeReference<List<ReadBillLogWithoutOrderResponse>>() {
+			});
+
+		model.addAttribute("myBillLogs", response.getBody());
+		model.addAttribute("page", "mybilllog");
+
+		return "index";
+	}
 }
