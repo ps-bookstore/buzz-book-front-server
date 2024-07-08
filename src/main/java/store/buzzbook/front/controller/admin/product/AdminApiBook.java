c03d7c87d57c8656c96052c94627e1d6aba26612
huijun98 <heej1998@gmail.com>
2024-07-08 19:30:06 +0900

    fix : api-book.html 수정

--- src/main/java/store/buzzbook/front/controller/admin/product/AdminApiBook.java
+++ src/main/java/store/buzzbook/front/controller/admin/product/AdminApiBook.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -33,5 +34,15 @@ public class AdminApiBook {
 		model.addAttribute("books", books);
 		model.addAttribute("query", query);
 		return "admin/pages/product-manage-api-book";
+	}
+
+	@PostMapping("/search")
+	public String searchAndSaveBooks(@RequestParam(required = false, defaultValue = "") String query, Model model) {
+		log.info("Search and save query: {}", query);
+		bookService.searchAndSaveBooks(query);
+		List<BookApiRequest.Item> books = bookService.searchBooks(query);
+		model.addAttribute("books", books);
+		model.addAttribute("query", query);
+		return "redirect:/admin/product";
 	}
 }
