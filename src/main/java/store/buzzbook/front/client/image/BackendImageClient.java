983af253d0ec5914fe54f457a20954a8eeac01d3
huijun98 <heej1998@gmail.com>
2024-07-09 21:09:17 +0900

    feat : 이미지 서버 임시구현

--- src/main/java/store/buzzbook/front/client/image/BackendImageClient.java
+++ src/main/java/store/buzzbook/front/client/image/BackendImageClient.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
-@FeignClient(name = "ImageClient", url = "http://${api.gateway.host}:${api.gateway.port}/api/image")
-public interface ImageClient {
+import java.util.List;
+
+@FeignClient(name = "backendImageClient", url = "http://${api.gateway.host}:${api.gateway.port}/api/image")
+public interface BackendImageClient {
 
 	@PostMapping(value = "/upload", consumes = "multipart/form-data")
-	ResponseEntity<String> uploadImage(
-		@RequestPart("file") MultipartFile file,
-		@RequestParam("folderPath") String folderPath
-	);
+	ResponseEntity<String> uploadImages(@RequestPart("files") List<MultipartFile> files,
+		@RequestParam("folderPath") String folderPath);
 }
