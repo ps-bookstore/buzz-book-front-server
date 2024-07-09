6a05a8be9ef92e1cb89ebd95320d9a996769362a
s49e <dydqja1ghk@hanmail.net>
2024-07-09 13:57:14 +0900

    save : 중간저장 2

--- src/main/java/store/buzzbook/front/dto/product/CategoryResponse.java
+++ src/main/java/store/buzzbook/front/dto/product/CategoryResponse.java
@@ -9,4 +9,14 @@ public class CategoryResponse {
 	private int id;
 	private String name;
 	private CategoryResponse parentCategory;
+
+	public String listToString() {
+		String result = this.name;
+
+		if (parentCategory != null) {
+			result = result + " > " + parentCategory.listToString();
+		}
+
+		return result;
+	}
 }
