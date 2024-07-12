aa3045cc90bc34b60f4aa87a633546ad0edd85bf
huijun98 <heej1998@gmail.com>
2024-07-12 09:45:39 +0900

    feat : 상품 출판일 내림차순으로 리스트 반환하도록 추가

--- src/main/java/store/buzzbook/front/controller/home/HomeController.java
+++ src/main/java/store/buzzbook/front/controller/home/HomeController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.RequiredArgsConstructor;
 import store.buzzbook.front.client.coupon.CouponPolicyClient;
+import store.buzzbook.front.client.product.ProductClient;
 import store.buzzbook.front.client.user.UserClient;
 import store.buzzbook.front.dto.coupon.CouponPoliciesResponse;
+import store.buzzbook.front.dto.product.ProductResponse;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,15 +19,20 @@ public class HomeController {
 
 	private final UserClient userClient;
 	private final CouponPolicyClient couponPolicyClient;
+	private final ProductClient productClient;
 
 	@GetMapping("home")
 	public String home(Model model) {
 		CouponPoliciesResponse couponPolicies = couponPolicyClient.getCouponPoliciesByScope(
 			List.of("global", "book", "category"));
 
+		//양 적으면 숫자 추가하세요
+		List<ProductResponse> latestProducts = productClient.getLatestProduct(5);
+
 		model.addAttribute("page", "main");
 		model.addAttribute("title", "메인페이지");
 		model.addAttribute("couponPolicies", couponPolicies);
+		model.addAttribute("latestProducts", latestProducts);
 		return "index";
 	}
 }
