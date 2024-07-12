e1ca2c6a1f9e652d83d7e829e4edff4cb0e102ee
Banana-Master <tjdgh0313@gmail.com>
2024-07-12 09:10:17 +0900

    버그 수정

--- src/main/java/store/buzzbook/front/controller/user/RegisterController.java
+++ src/main/java/store/buzzbook/front/controller/user/RegisterController.java
@@ -1,6 +1,7 @@
 package store.buzzbook.front.controller.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
