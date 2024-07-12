9562f8b34952eec83b064ed8d577937997ff1e83
s49e <dydqja1ghk@hanmail.net>
2024-07-12 23:36:19 +0900

    chore : import수정

--- src/main/java/store/buzzbook/front/controller/admin/product/AdminCategoryController.java
+++ src/main/java/store/buzzbook/front/controller/admin/product/AdminCategoryController.java
@@ -1,16 +1,12 @@
 package store.buzzbook.front.controller.admin.product;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
