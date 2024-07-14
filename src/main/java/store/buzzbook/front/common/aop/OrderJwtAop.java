b8d6367e56de37b7f271559af1d47fc0f3c2806c
ParkSeolDev <parkseol.dev@gmail.com>
2024-07-14 12:02:21 +0900

    fix: 관리자 주문 내역 aop 수정

--- src/main/java/store/buzzbook/front/common/aop/OrderJwtAop.java
+++ src/main/java/store/buzzbook/front/common/aop/OrderJwtAop.java
@@ -48,4 +48,31 @@ public class OrderJwtAop {
 			request.setAttribute(JwtService.ROLE, role);
 		}
 	}
+
+	@Before("@annotation(store.buzzbook.front.common.annotation.OrderAdminJwtValidate)")
+	public void adminAuthenticated(JoinPoint joinPoint) throws Throwable {
+
+		Optional<Cookie> authorizationHeader =cookieUtils.getCookie(request, CookieUtils.COOKIE_JWT_ACCESS_KEY);
+		Optional<Cookie> refreshHeader =cookieUtils.getCookie(request, CookieUtils.COOKIE_JWT_REFRESH_KEY);
+
+		if (authorizationHeader.isPresent() || refreshHeader.isPresent()) {
+			Map<String, Object> claims = jwtService.getInfoMapFromJwt(request, response);
+
+			Long userId = ((Integer)claims.get(JwtService.USER_ID)).longValue();
+			String loginId = (String)claims.get(JwtService.LOGIN_ID);
+			String role = (String)claims.get(JwtService.ROLE);
+
+			if (Objects.isNull(userId) || Objects.isNull(loginId) || Objects.isNull(role)) {
+				throw new AuthorizeFailException("user info가 null입니다.");
+			}
+
+			if (!"admin".equals(role)) {
+				throw new AuthorizeFailException("접근 권한이 없습니다.");
+			}
+
+			request.setAttribute(JwtService.USER_ID, userId);
+			request.setAttribute(JwtService.LOGIN_ID, loginId);
+			request.setAttribute(JwtService.ROLE, role);
+		}
+	}
 }
