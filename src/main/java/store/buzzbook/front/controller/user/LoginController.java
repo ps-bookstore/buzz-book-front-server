b2d8be1862845662151344496dc9d0d72f79639d
Banana-Master <tjdgh0313@gmail.com>
2024-07-16 01:38:07 +0900

    style css

--- src/main/java/store/buzzbook/front/controller/user/LoginController.java
+++ src/main/java/store/buzzbook/front/controller/user/LoginController.java
@@ -1,6 +1,5 @@
 package store.buzzbook.front.controller.user;
 
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
