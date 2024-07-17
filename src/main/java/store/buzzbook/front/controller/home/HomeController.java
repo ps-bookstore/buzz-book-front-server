5b7f4473569b3e71c180492e60e1efced6b4e5ca
huijun98 <heej1998@gmail.com>
2024-07-17 10:30:28 +0900

    fix : 디자인 수정

--- src/main/java/store/buzzbook/front/controller/home/HomeController.java
+++ src/main/java/store/buzzbook/front/controller/home/HomeController.java
@@ -1,6 +1,7 @@
 package store.buzzbook.front.controller.home;
 
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,12 +26,25 @@ public class HomeController {
 			List.of("global", "book", "category"));
 
 		//양 적으면 숫자 추가하세요
-		List<ProductResponse> latestProducts = productClient.getLatestProduct(5);
+		List<ProductResponse> latestProducts = productClient.getLatestProduct(8);
+
+		// 랜덤 상품 선택
+		ProductResponse randomProduct = getRandomProduct(latestProducts);
 
 		model.addAttribute("page", "main");
 		model.addAttribute("title", "메인페이지");
 		model.addAttribute("couponPolicies", couponPolicies);
 		model.addAttribute("latestProducts", latestProducts);
+		model.addAttribute("randomProduct", randomProduct);
 		return "index";
+	}
+
+	private ProductResponse getRandomProduct(List<ProductResponse> products) {
+		if (products.isEmpty()) {
+			return null;
+		}
+		Random random = new Random();
+		int randomIndex = random.nextInt(products.size());
+		return products.get(randomIndex);
 	}
 }
