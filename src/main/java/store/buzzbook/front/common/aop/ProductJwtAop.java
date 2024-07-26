0460177ee9b5ee0124976a31f5ed81d5f74a54ac
huijun98 <heej1998@gmail.com>
2024-07-26 11:02:14 +0900

    test : ProductJwtAop 테스트코드 추가

--- src/main/java/store/buzzbook/front/common/aop/ProductJwtAop.java
+++ src/main/java/store/buzzbook/front/common/aop/ProductJwtAop.java
@@ -4,13 +4,11 @@ package store.buzzbook.front.common.aop;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -40,21 +38,23 @@ public class ProductJwtAop {
 
 		Map<String, Object> claims = jwtService.getInfoMapFromJwt(request, response);
 
-		Long userId = claims.get(JwtService.USER_ID) instanceof Integer ? ((Integer) claims.get(JwtService.USER_ID)).longValue() : (Long) claims.get(JwtService.USER_ID);
-		String loginId = (String) claims.get(JwtService.LOGIN_ID);
-		String role = (String) claims.get(JwtService.ROLE);
+		try{
+			Long userId = ((Integer)claims.get(JwtService.USER_ID)).longValue();
+			String loginId = (String)claims.get(JwtService.LOGIN_ID);
+			String role = (String)claims.get(JwtService.ROLE);
 
-		if (Objects.isNull(userId) || Objects.isNull(loginId) || Objects.isNull(role)) {
-			throw new AuthorizeFailException("사용자 정보가 null 입니다.");
+			if (!"ADMIN".equals(role)) {
+				log.info("{}",role);
+				throw new AuthorizeFailException("접근 권한이 없습니다.");
+			}
+
+			request.setAttribute(JwtService.USER_ID, userId);
+			request.setAttribute(JwtService.LOGIN_ID, loginId);
+			request.setAttribute(JwtService.ROLE, role);
+
+		}catch (NullPointerException e) {
+			throw new AuthorizeFailException("user info가 null입니다.");
+
 		}
-
-		if (!"ADMIN".equals(role)) {
-			log.info("{}",role);
-			throw new AuthorizeFailException("접근 권한이 없습니다.");
-		}
-
-		request.setAttribute(JwtService.USER_ID, userId);
-		request.setAttribute(JwtService.LOGIN_ID, loginId);
-		request.setAttribute(JwtService.ROLE, role);
 	}
 }
