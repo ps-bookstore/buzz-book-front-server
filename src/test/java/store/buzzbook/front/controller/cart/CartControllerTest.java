f343041a645d3caff00270875665e3f81c282b0d
Heldennar <krain7127@gmail.com>
2024-07-26 10:46:51 +0900

    test : test 코드 추가

--- src/test/java/store/buzzbook/front/controller/cart/CartControllerTest.java
+++ src/test/java/store/buzzbook/front/controller/cart/CartControllerTest.java
@@ -1,6 +1,7 @@
 package store.buzzbook.front.controller.cart;
 
 import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -10,22 +11,24 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.servlet.http.HttpServletRequest;
 import store.buzzbook.front.common.interceptor.CartInterceptor;
 import store.buzzbook.front.dto.cart.CartDetailResponse;
+import store.buzzbook.front.dto.cart.CreateCartDetailRequest;
 import store.buzzbook.front.service.cart.CartService;
 
 @ActiveProfiles("test")
-@WebMvcTest(CartController.class)
+@WebMvcTest({CartController.class, CartRestController.class})
 class CartControllerTest {
 	@MockBean
 	private CartInterceptor cartInterceptor;
@@ -33,13 +36,17 @@ class CartControllerTest {
 	private MockMvc mockMvc;
 	@MockBean
 	private CartService cartService;
+	@Autowired
+	private ObjectMapper objectMapper;
 
 	private String uuid;
 	private Long detailId;
 	private List<CartDetailResponse> cartDetailResponses;
 
 	@BeforeEach
-	void setUp() {
+	void setUp() throws Exception {
+		when(cartInterceptor.preHandle(any(),any(),any())).thenReturn(true);
+
 		detailId = 1L;
 		uuid = "sample-uuid";
 
@@ -94,6 +101,7 @@ class CartControllerTest {
 		when(cartService.getCartIdFromRequest(any(HttpServletRequest.class))).thenReturn(uuid);
 
 		mockMvc.perform(post("/cart")
+				.with(csrf())
 				.param("id", detailId.toString())
 				.param("quantity", String.valueOf(cartDetailResponses.getFirst().getQuantity())))
 			.andExpect(status().is3xxRedirection())
@@ -105,12 +113,32 @@ class CartControllerTest {
 	void testDeleteCart() throws Exception {
 		when(cartService.getCartIdFromRequest(any(HttpServletRequest.class))).thenReturn(uuid);
 
-		mockMvc.perform(delete("/cart"))
+		mockMvc.perform(delete("/cart").with(csrf()))
 			.andExpect(status().isOk())
 			.andExpect(view().name("index"))
 			.andExpect(model().attributeExists("cart"))
 			.andExpect(model().attribute("page", "cart"))
 			.andExpect(model().attribute("title", "장바구니"))
 			.andExpect(model().attribute("cart", List.of()));
+	}
+
+	@WithMockUser
+	@Test
+	void testAddCart() throws Exception {
+		CreateCartDetailRequest createCartDetailRequest = new CreateCartDetailRequest(
+			10,
+			2
+		);
+		when(cartService.getCartIdFromRequest(any(HttpServletRequest.class))).thenReturn(uuid);
+		doNothing().when(cartService).createCartDetail(uuid, createCartDetailRequest);
+
+		mockMvc.perform(post("/cart/detail").with(csrf())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(createCartDetailRequest)))
+			.andExpect(status().isOk());
+
+		verify(cartService, times(1)).createCartDetail(anyString(), any(CreateCartDetailRequest.class));
+		verify(cartService, times(1)).getCartIdFromRequest(any(HttpServletRequest.class));
+
 	}
 }
