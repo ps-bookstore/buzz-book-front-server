d277011f3b9e26a89630114efbdded288e235d42
ParkSeolDev <parkseol.dev@gmail.com>
2025-02-18 19:16:43 +0900

    chore: 수정

--- src/main/java/store/buzzbook/front/dto/payment/TossPaymentResponse.java
+++ src/main/java/store/buzzbook/front/dto/payment/TossPaymentResponse.java
@@ -1,9 +1,11 @@
 package store.buzzbook.front.dto.payment;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
 public class TossPaymentResponse extends PayResult {
 
